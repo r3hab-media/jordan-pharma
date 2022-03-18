3204ab2d2240ed0d7d3aa38afebe2645190cd420
r3hab-media <jared.newnam@gmail.com>
2022-03-18 12:36:32 -0700

    updated scss files, rewrote nav and behavior

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+var body = document.querySelector("body");
+var openMenu = document.querySelectorAll(".menuControl, .menu nav a");
+
+for (var i = 0; i < openMenu.length; i++) {
+  openMenu[i].addEventListener("click", function (e) {
+    body.classList.toggle("menu--active");
+  });  
+}
