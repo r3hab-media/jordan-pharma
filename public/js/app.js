ff65115ff9f26b35e24c2b469b6a4760c8ec237d
r3hab-media <jared.newnam@gmail.com>
2022-04-13 21:26:52 -0700

    various updates

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,17 @@
+// const id = "base-url";
+const getPort = window.location.port;
+const devBase = 'http://r3hab-media:5500/public/';
+const prodBase = 'https://jordan-pharma-dev.web.app';
+const base = document.createElement('base');
+
+if (getPort === '5500') {
+  base.href = devBase;
+  document.getElementsByTagName("head")[0].appendChild(base);
+} else {
+  base.href = prodBase;
+  document.getElementsByTagName("head")[0].appendChild(base);
+}
+
 var body = document.querySelector("body");
 var openMenu = document.querySelectorAll(".menuControl, .menu nav a");
 
@@ -9,3 +23,4 @@ for (var i = 0; i < openMenu.length; i++) {
 
 var currYr = new Date().getFullYear();
 thisYear.textContent = currYr; 
+
