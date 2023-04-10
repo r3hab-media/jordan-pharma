cca2f7d67977c9e81434d41f5fd312c727f84277
r3hab-media <jared.newnam@gmail.com>
2023-04-09 18:46:24 -0700

    refactored javascript

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,22 @@
-// const id = "base-url";
-// const getPort = window.location.port;
-// const devBase = 'http://r3hab-media:5500/public/';
-// const prodBase = 'https://jordan-pharma.com';
-// const base = document.createElement('base');
+// Utility functions
+const forEach = (elements, callback) => Array.from(elements).forEach(callback);
 
-// if (getPort === '5500') {
-//   base.href = devBase;
-//   document.getElementsByTagName("head")[0].appendChild(base);
-// } else {
-//   base.href = prodBase;
-//   document.getElementsByTagName("head")[0].appendChild(base);
-// }
-
-//slides
+// Slides
 const sliderExist = document.querySelectorAll('.slider-box').length > 0;
 
 if (sliderExist) {
   let slideIndex = 0;
-  document.querySelector('.slideshow') ? showSlides() : null;
+  document.querySelector('.slideshow') && showSlides();
   
   function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("slideshow__slide");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex-1].style.display = "block";    
-    setTimeout(showSlides, 4000); // Change image every 2 seconds
+    const slides = document.getElementsByClassName("slideshow__slide");
+    forEach(slides, (slide) => slide.style.display = "none");
+    slideIndex = (slideIndex + 1) % slides.length;
+    slides[slideIndex].style.display = "block";    
+    setTimeout(showSlides, 4000);
   }
   
-  window.onresize = function resizeHandler() {
+  window.onresize = function() {
     const left = document.querySelector(".slider-box__left--content");
     const right = document.querySelector(".slider-box__right");
     right.style.height = `${left.clientHeight}px`;
@@ -39,58 +24,45 @@ if (sliderExist) {
   };  
 }
 
-//accordion
-let acc = document.getElementsByClassName("accordion");
-let j;
-
-for (j = 0; j < acc.length; j++) {
-  acc[j].addEventListener("click", function() {
+// Accordion
+const acc = document.getElementsByClassName("accordion");
+forEach(acc, (elem) => {
+  elem.addEventListener("click", function() {
     this.classList.toggle("active");
     let panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight * 1.2 + "px";
-    } 
+    panel.style.maxHeight = panel.style.maxHeight ? null : panel.scrollHeight * 1.2 + "px";
   });
-}
+});
 
+// Quotes
 const quotesExist = document.querySelectorAll('.myQuotes').length > 0;
 
 if (quotesExist) {
   window.onload = function() {
-    setInterval(function() {
-      plusQuotes(1);
-    }, 20000);
+    setInterval(() => plusQuotes(1), 20000);
   }
   
-  var quoteIndex = 1;
-  showQuotes(quoteIndex);
-  
+  let quoteIndex = 0;
+
+  function showQuotes() {
+    const quotes = document.getElementsByClassName("myQuotes");
+    forEach(quotes, (quote) => quote.style.display = "none");
+    quotes[quoteIndex].style.display = "block";
+  }
+
   function plusQuotes(n) {
-    showQuotes(quoteIndex += n);
+    const quotes = document.getElementsByClassName("myQuotes");
+    quoteIndex = (quoteIndex + n) % quotes.length;
+    showQuotes();
   }
   
-  function currentQuote(n) {
-    showQuotes(quoteIndex = n);
-  }
-
-  function showQuotes(n) {
-    var i;
-    var quotes = document.getElementsByClassName("myQuotes");
-    if (n > quotes.length) {quoteIndex = 1}    
-    if (n < 1) {quoteIndex = quotes.length}
-    for (i = 0; i < quotes.length; i++) {
-      quotes[i].style.display = "none";  
-    }
-    quotes[quoteIndex-1].style.display = "block";
-  }
+  showQuotes();
 }
 
-newPageTitle = document.querySelector('main h3').textContent;
-changePageTitle = () => {  
+// Page title
+const changePageTitle = () => {
+  const newPageTitle = document.querySelector('main h3').textContent;
   document.querySelector('title').textContent = `Jordan Pharma Compliance | ${newPageTitle}`;
-}
-window.addEventListener('onload', changePageTitle());
+};
 
-
+window.addEventListener('load', changePageTitle);
